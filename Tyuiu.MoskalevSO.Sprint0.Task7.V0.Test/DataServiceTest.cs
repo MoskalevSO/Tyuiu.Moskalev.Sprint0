598b6cc3507d72c47ef0903ea114e882b9cac887
sr <omronaski12@gmail.com>
2026-09-17 03:38:17 +0500

    Task7

---
+++ Tyuiu.MoskalevSO.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MoskalevSO.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.MoskalevSO.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChechAdditionArraysValid()
+        {
+            int[] nums1 = { 1, 1, 1, 1, 1 };
+            int[] nums2 = { 1, 1, 1, 1, 1 };
+            int[] nums3 = { 2, 2, 2, 2, 2 };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+
+        }
+    }
+}
