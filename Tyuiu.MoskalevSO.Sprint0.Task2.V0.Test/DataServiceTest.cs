c53f8357b2ee0b92babec1d5a1a5b9c5748c27e0
sr <omronaski12@gmail.com>
2026-09-16 23:12:08 +0500

    Task2

---
+++ Tyuiu.MoskalevSO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MoskalevSO.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MoskalevSO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "рыба";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., рыба", res);
+        }
+    }
+}
