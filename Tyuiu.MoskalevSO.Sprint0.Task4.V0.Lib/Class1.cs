d2bec73eeb9bb8cbf2f288b7ab8a844bee181ebb
sr <omronaski12@gmail.com>
2026-09-17 01:03:23 +0500

    Task4

---
+++ Tyuiu.MoskalevSO.Sprint0.Task4.V0.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.MoskalevSO.Sprint0.Task4.V0.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.MoskalevSO.Sprint0.Task4.V0/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevSO.Sprint0.Task4.V0
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
